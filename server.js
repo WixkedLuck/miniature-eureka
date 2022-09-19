171183fc54c1ceeea1882923fedfa91da3629361
David Hybner <dah1998@yahoo.com>
2022-09-19 14:16:19 -0400

    moving route up in code

--- server.js
+++ server.js
@@ -51,13 +51,14 @@ res.json(req.body);
 
 
 // Routes for html 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname,"/public/index.html"));
+});
+
 app.get('/notes',(req,res) => {
     res.sendFile(path.join(__dirname,"/public/notes.html"));
 });
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,"/public/index.html"));
-});
 
 app.get('*', (req,res) => {
     res.sendFile(path.join(__dirname,"/public/index.html"));
