e17fc9a98196ffed738bdc563737a3b634eec9cc
David Hybner <dah1998@yahoo.com>
2022-09-19 14:12:45 -0400

    updated route

--- server.js
+++ server.js
@@ -55,6 +55,10 @@ app.get('/notes',(req,res) => {
     res.sendFile(path.join(__dirname,"/public/notes.html"));
 });
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname,"/public/index.html"));
+});
+
 app.get('*', (req,res) => {
     res.sendFile(path.join(__dirname,"/public/index.html"));
 });
