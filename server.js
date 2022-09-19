9e13d028babda2c58c3b4b3625ea4ca82f5ce4b5
David Hybner <dah1998@yahoo.com>
2022-09-19 14:20:32 -0400

    updated PORT variable

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express= require('express');
 const notes = require('./db/db.json');
 const fs = require('fs'); 
 const path = require('path'); 
-const PORT = process.env.Port || 3001; 
+const PORT = process.env.PORT || 3001; 
 const app =express();
 
 // Sets up the Express app to handle data parsing
